6c4dbc8e7a72f7c3add5bc4be46aa9fcbbce203e
Mikhail Vyuzhanin <miv@level.travel>
2022-01-06 17:40:49 +0300

    add statuses with state machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,31 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+
+  state_machine :initial => :new_task do
+    event :develop do
+      transition [:new_task, :in_code_review, :in_qa] => :in_development
+    end
+
+    event :test do
+      transition :in_development => :in_qa
+    end
+
+    event :review do
+      transition :in_qa => :in_code_review
+    end
+
+    event :ready do
+      transition :in_code_review => :ready_for_release
+    end
+
+    event :archive do
+      transition [:new_task, :released] => :archived
+    end
+
+    event :release do
+      transition :ready_for_release => :released
+    end
+  end
 end
